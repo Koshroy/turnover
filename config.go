0af666f2c1ae025603f6be2c3393716135ee4cab
Koushik Roy <koushik@meff.me>
2018-11-21 15:57:48 -0800

    Add public and private key options to config

--- config.go
+++ config.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
+	
 	"github.com/BurntSushi/toml"
 )
 
 //ServerConfig defines config options for running the server
 type ServerConfig struct {
-	Hostname string
+	Hostname   string
+	PublicKey  string `toml:"public_key"`
+	PrivateKey string `toml:"private_key"`
 }
 
 //Config is the config object
@@ -17,10 +21,37 @@ type Config struct {
 // LoadConfig loads a config at configPath
 func LoadConfig(configPath string) (*Config, error) {
 	var conf Config
-	_, err := toml.DecodeFile(configPath, &conf)
+	md, err := toml.DecodeFile(configPath, &conf)
+	if err != nil {
+		return nil, err
+	}
+
+	undecoded := md.Undecoded()
+	if len(undecoded) != 0 {
+		return nil, fmt.Errorf("these config fields are unused: %q", undecoded)
+	}
+
+	err = ValidateConfig(conf)
 	if err != nil {
 		return nil, err
 	}
 
 	return &conf, nil
+}
+
+// ValidateConfig validates a Config
+func ValidateConfig(conf Config) error {
+	if conf.Server.Hostname == "" {
+		return fmt.Errorf("no hostname given")
+	}
+
+	if conf.Server.PublicKey == "" {
+		return fmt.Errorf("no public key path given")
+	}
+
+	if conf.Server.PrivateKey == "" {
+		return fmt.Errorf("no private key path given")
+	}
+
+	return nil
 }
--- config_test.go
+++ config_test.go
@@ -11,6 +11,8 @@ func TestLoadConfig(t *testing.T) {
 	configData := `
         [server]
         hostname = "example.com"
+        public_key = "example.key"
+        private_key = "example.pem"
         `
 
 	var config Config
@@ -20,9 +22,27 @@ func TestLoadConfig(t *testing.T) {
 		t.Errorf("could not parse example config properly")
 	}
 
+	err = ValidateConfig(config)
+
+	if err != nil {
+		t.Errorf("could not validate config: %v", err)
+	}
+
 	if config.Server.Hostname != "example.com" {
 		t.Errorf(
 			"config hostname expected example.com got: %s", config.Server.Hostname,
+		)
+	}
+
+	if config.Server.PublicKey != "example.key" {
+		t.Errorf(
+			"config public_key expected example.key got: %s", config.Server.PublicKey,
+		)
+	}
+
+	if config.Server.PrivateKey != "example.pem" {
+		t.Errorf(
+			"config private_key expected example.pem got: %s", config.Server.PrivateKey,
 		)
 	}
 }
