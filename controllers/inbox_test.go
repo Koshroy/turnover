973b4b12319c8b04642f8bb9ba90cbace2aa9dfe
Koushik Roy <koushik@meff.me>
2018-12-26 13:14:36 -0800

    Factor out the mock transport

--- controllers/inbox_test.go
+++ controllers/inbox_test.go
@@ -1,14 +1,13 @@
 package controllers
 
 import (
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	"os"
 	"strings"
 	"testing"
-	"time"
+
+	"github.com/Koshroy/turnover/util"
 )
 
 const followJSON = `{
@@ -91,43 +90,6 @@ const createNoteJSON = `{
 }
 `
 
-type mockTransport struct {
-	Fallback http.RoundTripper
-}
-
-func (m *mockTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	if req.URL.Host != "www.w3.org" && req.URL.Path != "/ns/activitystreams" {
-		return m.Fallback.RoundTrip(req)
-	}
-
-	f, err := os.Open("./testdata/activitystreams.jsonld")
-	if err != nil {
-		return nil, fmt.Errorf("error opening testdata for mock transport: %v", err)
-	}
-
-	s, err := f.Stat()
-	if err != nil {
-		return nil, fmt.Errorf("error getting filesize for mock transport: %v", err)
-	}
-
-	header := make(http.Header)
-	header.Add("Content-Length", fmt.Sprintf("%d", s.Size()))
-	header.Add("Content-Type", "application/ld+json")
-	header.Add("Date", s.ModTime().Format(time.RFC1123))
-
-	return &http.Response{
-		Status:        http.StatusText(http.StatusOK),
-		StatusCode:    http.StatusOK,
-		Proto:         req.Proto,
-		ProtoMajor:    req.ProtoMajor,
-		ProtoMinor:    req.ProtoMinor,
-		ContentLength: s.Size(),
-		Request:       req,
-		Header:        header,
-		Body:          f,
-	}, nil
-}
-
 type respTest struct {
 	JSONInput  string
 	StatusCode int
@@ -159,7 +121,7 @@ func TestInboxHandler(t *testing.T) {
 	t.Parallel()
 
 	mockClient := &http.Client{
-		Transport: &mockTransport{Fallback: http.DefaultTransport},
+		Transport: &util.MockTransport{Fallback: http.DefaultTransport},
 	}
 	i := NewInbox([]string{}, "https", "www.example.com", mockClient)
 
